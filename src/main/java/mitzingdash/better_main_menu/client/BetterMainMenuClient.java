913faa04ae3c58230d2528d693917e95c82dd041
Mitzingdash <wearwolfgamer99@gmail.com>
2024-02-17 13:29:06 +0100

    adding stuff

--- src/main/java/mitzingdash/better_main_menu/client/BetterMainMenuClient.java
+++ src/main/java/mitzingdash/better_main_menu/client/BetterMainMenuClient.java
@@ -1,5 +1,12 @@
 package mitzingdash.better_main_menu.client;
 
-public class BetterMainMenuClient {
+import net.fabricmc.api.ClientModInitializer;
+
+public class BetterMainMenuClient implements ClientModInitializer {
+	@Override
+    public void onInitializeClient() {
+    	//check if "Better Statistics Screen" is installed
+		
+	}
 	
 }
