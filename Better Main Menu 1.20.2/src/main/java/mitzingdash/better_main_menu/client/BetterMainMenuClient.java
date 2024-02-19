7ea2436baa20beaec17a1d83350e8f8d8c4eb8a6
Mitzingdash <wearwolfgamer99@gmail.com>
2024-02-19 13:58:19 +0100

    added version support

---
+++ Better Main Menu 1.20.2/src/main/java/mitzingdash/better_main_menu/client/BetterMainMenuClient.java
@@ -0,0 +1,12 @@
+package mitzingdash.better_main_menu.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BetterMainMenuClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+
+    }
+
+}
