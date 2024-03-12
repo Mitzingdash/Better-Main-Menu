2c47800fa8b34f1febfbc4cb1a172d6891e42fdb
Mitzingdash <wearwolfgamer99@gmail.com>
2024-03-12 16:22:48 +0100

    bugfix

--- Better Main Menu 1.20.4/src/main/java/mitzingdash/better_main_menu/client/gui/screen/BmmConfigScreen.java
+++ Better Main Menu 1.20.4/src/main/java/mitzingdash/better_main_menu/client/gui/screen/BmmConfigScreen.java
@@ -1,9 +1,18 @@
 package mitzingdash.better_main_menu.client.gui.screen;
 
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+
 import io.github.thecsdev.tcdcommons.api.client.gui.screen.TScreenPlus;
 import net.minecraft.text.Text;
 
-public class BmmConfigScreen extends TScreenPlus {
+public class BmmConfigScreen extends TScreenPlus implements ModMenuApi {
+
+	@Override
+	public ConfigScreenFactory<?> getModConfigScreenFactory() {
+		// TODO Auto-generated method stub
+		return parent -> new BmmConfigScreen().getAsScreen();
+	}
 
 	public BmmConfigScreen() {
 		super(Text.translatable("narrator.screen.title"));
