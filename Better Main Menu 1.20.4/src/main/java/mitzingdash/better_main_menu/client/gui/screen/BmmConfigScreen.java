3c9517ac4e1a38f31ee35c938d6f62b607d2e7bf
Mitzingdash <wearwolfgamer99@gmail.com>
2024-03-12 17:00:29 +0100

    Update BmmConfigScreen.java

--- Better Main Menu 1.20.4/src/main/java/mitzingdash/better_main_menu/client/gui/screen/BmmConfigScreen.java
+++ Better Main Menu 1.20.4/src/main/java/mitzingdash/better_main_menu/client/gui/screen/BmmConfigScreen.java
@@ -4,18 +4,22 @@ import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 
 import io.github.thecsdev.tcdcommons.api.client.gui.screen.TScreenPlus;
+import io.github.thecsdev.tcdcommons.api.client.util.interfaces.IParentScreenProvider;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
 
-public class BmmConfigScreen extends TScreenPlus implements ModMenuApi {
+public class BmmConfigScreen extends TScreenPlus implements ModMenuApi, IParentScreenProvider {
 
+	public Screen parent = null;
 	@Override
 	public ConfigScreenFactory<?> getModConfigScreenFactory() {
 		// TODO Auto-generated method stub
-		return parent -> new BmmConfigScreen().getAsScreen();
+		return parent -> new BmmConfigScreen(parent).getAsScreen();
 	}
 
-	public BmmConfigScreen() {
+	public BmmConfigScreen(Screen parent) {
 		super(Text.translatable("narrator.screen.title"));
+		this.parent = parent;
 		// TODO Auto-generated constructor stub
 	}
 
@@ -23,6 +27,17 @@ public class BmmConfigScreen extends TScreenPlus implements ModMenuApi {
 	protected void init() {
 		// TODO Auto-generated method stub
 		
+	}
+
+	@Override
+	public Screen getParentScreen() {
+		// TODO Auto-generated method stub
+		return this.parent;
+	}
+
+	@Override
+	public void close() {
+		getClient().setScreen(this.parent);
 	}
 
 }
