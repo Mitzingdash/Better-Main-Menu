39ac4042f2a995f33b73be1efa068bbc2a980307
Mitzingdash <90938266+Mitzingdash@users.noreply.github.com>
2024-03-19 20:49:09 +0100

    Update BmmConfigScreen.java

--- Better Main Menu 1.20.4/src/main/java/mitzingdash/better_main_menu/client/gui/screen/BmmConfigScreen.java
+++ Better Main Menu 1.20.4/src/main/java/mitzingdash/better_main_menu/client/gui/screen/BmmConfigScreen.java
@@ -3,6 +3,8 @@ package mitzingdash.better_main_menu.client.gui.screen;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 
+import io.github.thecsdev.tcdcommons.api.client.gui.other.TFillColorElement;
+import io.github.thecsdev.tcdcommons.api.client.gui.panel.TPanelElement;
 import io.github.thecsdev.tcdcommons.api.client.gui.screen.TScreenPlus;
 import io.github.thecsdev.tcdcommons.api.client.util.interfaces.IParentScreenProvider;
 import net.minecraft.client.gui.screen.Screen;
@@ -26,7 +28,12 @@ public class BmmConfigScreen extends TScreenPlus implements ModMenuApi, IParentS
 	@Override
 	protected void init() {
 		// TODO Auto-generated method stub
+		var contentPane = new TFillColorElement(10, 10, getWidth()-20, getHeight()-20);
+		addChild(contentPane);
 		
+		var panel = new TPanelElement(2, 2, getWidth()-4, getHeight()-4);
+		panel.setOutlineColor(0x44000000);
+		contentPane.addChild(panel);
 	}
 
 	@Override
